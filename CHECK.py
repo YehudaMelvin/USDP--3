dccf0c26625aab7b868572cb0ef6e3868ae589ec
YehudaMelvin <92035492+YehudaMelvin@users.noreply.github.com>
2023-03-28 21:37:07 +0700

    Delete CHECK.py

--- CHECK.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-root=Tk()
-root.title('Convertor')
-root.geometry('1280x720')
-
-Label(root,text='conversion system', font=('times new rommon ',60,'bold'), bg='lime').pack()
-
-root.mainloop
